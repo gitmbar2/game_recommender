6530ac2efc0349382c0cb64558ec57bc14e52f69
Michael Barton <mbartonenglish@gmail.com>
2018-01-09 17:15:38 -0600

    Try MinMax standardization

--- src/EDA.py
+++ src/EDA.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def load_200k():
@@ -7,11 +8,16 @@ def load_200k():
     steam_200k_df = steam_200k_df[steam_200k_df['purchase_action'] == 'play']
     return steam_200k_df
 
-def load_200k_n_games_played(n):
+def load_without_cold_start(min_games=5, min_users=0):
     steam_df = load_200k()
+    # filter users
     game_counts = steam_df.groupby('uid').count()
-    usable_users = game_counts[game_counts['game_name'] >= 5].reset_index()
-    return steam_df[steam_df['uid'].isin(usable_users['uid'].values)]
+    usable_users = game_counts[game_counts['game_name'] >= min_games].reset_index()
+    filtered_users = steam_df[steam_df['uid'].isin(usable_users['uid'].values)]
+    # filter games
+    user_counts = steam_df.groupby('game_name').count()
+    usable_games = user_counts[user_counts['playtime'] > min_users].reset_index()
+    return filtered_users[steam_df['game_name'].isin(usable_games['game_name'].values)]
 
 def rank_playtime(time):
     if time <= 1:
@@ -21,3 +27,19 @@ def rank_playtime(time):
     if time > 6 and time <= 25:
         return 2
     return 3
+
+def add_summaries(df, max_rank=3):
+    # Do min-max normalization
+    def min_max(row):
+        # TODO double check this
+        if (row['playtime_max'] == row['playtime_min']):
+            return 1.0 / row['game_counts']
+        diff = row['playtime_max'] - row['playtime_min']
+        return (row['playtime'] - row['playtime_min']) / diff
+
+    aggs = {'playtime_mean': np.mean, 'playtime_min': np.min, 'playtime_max': np.max, 'game_counts': 'count'}
+    grouped_means = df.groupby('game_name').agg({'playtime': aggs})
+    grouped_means.columns = [col[1] for col in grouped_means.columns]
+    joined = df.join(grouped_means, on='game_name')
+    joined['min_max'] = joined.apply(lambda x: min_max(x) * max_rank, axis=1)
+    return joined
